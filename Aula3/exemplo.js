2caddcc2383cf40096e88dc3925e6901b3fd0979
Renan Costa Filgueiras <renan2727@hotmail.com>
2023-02-28 11:17:17 -0300

    Código finalizado

--- Aula3/exemplo.js
+++ Aula3/exemplo.js
@@ -9,7 +9,16 @@ function somaNumeros(){
     var num1 = parseInt(document.getElementById("num1").value,10);
     var num2 = document.getElementById("num2").value;
     num2 = parseInt(num2,10);
-    //soma = num1 + num2;
-    alert("Soma: "+(num1+num2));
+    soma = num1+num2;
+    if(isNaN(soma)){
+        alert("Valor informado não é um número");
+    }else{
+        alert("Soma: "+soma);
+    } 
 }
 
+function tratamentoNumero(num){
+    if(isNaN(num)){
+        alert("Valor informado não é um número");
+    }
+}
