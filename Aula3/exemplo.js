aa3d1c9230a6529c9892d1d4a36cfa0cff4c96cf
Renan Costa Filgueiras <renan2727@hotmail.com>
2023-02-28 10:38:59 -0300

    Somar números

--- Aula3/exemplo.js
+++ Aula3/exemplo.js
@@ -5,3 +5,11 @@ function exibirBemVindo(){
     alert("Olá mundo, "+nomeCompleto);
 }
 
+function somaNumeros(){
+    var num1 = parseInt(document.getElementById("num1").value,10);
+    var num2 = document.getElementById("num2").value;
+    num2 = parseInt(num2,10);
+    //soma = num1 + num2;
+    alert("Soma: "+(num1+num2));
+}
+
