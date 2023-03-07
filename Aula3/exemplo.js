1fc9d83a7e77818b4090f537290c719c12a0e077
Renan Costa Filgueiras <renan2727@hotmail.com>
2023-03-07 10:56:26 -0300

    Aula 03

--- Aula3/exemplo.js
+++ Aula3/exemplo.js
@@ -5,20 +5,26 @@ function exibirBemVindo(){
     alert("Olá mundo, "+nomeCompleto);
 }
 
+//Função que soma dois números, mas que obrigatoriamente os ids precisam ser num1 e num2
 function somaNumeros(){
-    var num1 = parseInt(document.getElementById("num1").value,10);
+    var num1 = window.prompt("Informe um número");
     var num2 = document.getElementById("num2").value;
     num2 = parseInt(num2,10);
     soma = num1+num2;
     if(isNaN(soma)){
         alert("Valor informado não é um número");
     }else{
-        alert("Soma: "+soma);
-    } 
+        document.getElementById("exemplo").innerHTML = "Somou";
+    }
+
 }
 
+/*
+    Função que valida se o valor informado é um número ou não
+ */
 function tratamentoNumero(num){
     if(isNaN(num)){
         alert("Valor informado não é um número");
     }
+    document.getElementById("exemplo").innerHTML = "É um número";
 }
