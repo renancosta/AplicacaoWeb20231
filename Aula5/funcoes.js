0e302e6d9adcd890022cbaed63e40a24e7cc58b0
Renan Costa Filgueiras <renan2727@hotmail.com>
2023-03-14 09:33:53 -0300

    Atualizando com botstrap e input file

--- Aula5/funcoes.js
+++ Aula5/funcoes.js
@@ -1,5 +1,5 @@
 function aplicarEfeitos(corFundo,caminhoImagem){
     document.bgColor=corFundo;
-    alert(caminhoImagem);
+    caminhoImagem = caminhoImagem.replace("C:\\fakepath\\","");
     document.images[0].src=caminhoImagem;
 }
