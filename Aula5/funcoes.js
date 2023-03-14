9dd0ae41753a8ab21be53c5f8e40793dadb0f927
Renan Costa Filgueiras <renan2727@hotmail.com>
2023-03-14 10:05:56 -0300

    Update funcoes.js

--- Aula5/funcoes.js
+++ Aula5/funcoes.js
@@ -1,5 +1,5 @@
 function aplicarEfeitos(corFundo,caminhoImagem){
-    document.bgColor=corFundo;
+    document.body.style.backgroundColor=corFundo;
     caminhoImagem = caminhoImagem.replace("C:\\fakepath\\","");
     document.images[0].src=caminhoImagem;
 }
